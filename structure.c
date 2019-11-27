e7fb996280055edb5b8ad5f417a2483a2cde02de
Kotaro7750 <7750koutarou@gmail.com>
2019-11-27 17:23:51 +0900

    make exceeding field

--- structure.c
+++ structure.c
@@ -7,19 +7,22 @@
 enum line_type { Top, Mid, Bottom };
 
 struct cell {
-  int size;
+  int unprinted_size;
+  int width;
   char *name;
   int name_len;
   struct cell *next;
+  bool is_exceed;
 };
 
 void print_help(void);
 struct cell *create_cell(int size, char *name, int name_len);
-int input_cells();
+bool input_cells();
 void print_cells(int);
 void print_count(struct cell **seek, int line, int bit_width);
 void print_line(struct cell **seek, char partition, char padding,
                 enum line_type line_type, int bit_width);
+void print_exceed();
 
 int get_numstr_length(int num);
 
@@ -60,8 +63,13 @@ int main(int argc, char *argv[]) {
 
   head = create_cell(0, "", 0);
 
-  int cell_num = input_cells();
+  bool is_exceed_cell_exist = input_cells();
   print_cells(bit_width);
+
+  if (is_exceed_cell_exist) {
+    printf("\n");
+    print_exceed();
+  }
   return 0;
 }
 
@@ -71,34 +79,40 @@ void print_help(void) {
   printf("\t-h: show help.\n");
 }
 
-struct cell *create_cell(int size, char *name, int name_len) {
+struct cell *create_cell(int width, char *name, int name_len) {
   struct cell *new_cell;
   new_cell = (struct cell *)calloc(1, sizeof(struct cell));
 
-  new_cell->size = size;
+  new_cell->unprinted_size = width;
+  new_cell->width = width;
   new_cell->name_len = name_len;
   new_cell->name = (char *)calloc(name_len + 1, sizeof(char));
 
   snprintf(new_cell->name, name_len + 1, "%s", name);
 
+  if (name_len >= width) {
+    new_cell->is_exceed = true;
+  } else {
+    new_cell->is_exceed = false;
+  }
   new_cell->next = NULL;
 
   return new_cell;
 }
 
-int input_cells() {
+bool input_cells() {
   struct cell *tmp;
   tmp = head;
-  int cell_num = 0;
+  bool is_exceed_cell_exist = false;
 
   while (1) {
-    int size;
+    int width;
     char name[255];
     int name_len;
 
-    printf("size? :");
-    scanf("%d", &size);
-    if (size <= 0) {
+    printf("width? :");
+    scanf("%d", &width);
+    if (width <= 0) {
       break;
     }
 
@@ -108,13 +122,14 @@ int input_cells() {
 
     name_len = strlen(name);
 
-    tmp->next = create_cell(size, name, name_len);
+    tmp->next = create_cell(width, name, name_len);
     tmp = tmp->next;
-
-    cell_num++;
+    if (tmp->is_exceed) {
+      is_exceed_cell_exist = true;
+    }
   }
 
-  return cell_num;
+  return is_exceed_cell_exist;
 }
 
 void print_cells(int bit_width) {
@@ -133,7 +148,7 @@ void print_cells(int bit_width) {
     print_line(&mid, '|', ' ', Mid, bit_width);
     print_line(&bottom, '+', '-', Bottom, bit_width);
 
-    if (bottom != NULL && bottom->size > 0) {
+    if (bottom != NULL && bottom->unprinted_size > 0) {
       line++;
     }
   }
@@ -142,20 +157,20 @@ void print_cells(int bit_width) {
 void print_count(struct cell **seek, int line, int bit_width) {
   int line_i = 0;
   while (line_i < bit_width) {
-    if ((*seek)->size <= 0) {
+    if ((*seek)->unprinted_size <= 0) {
       (*seek) = (*seek)->next;
       break;
     }
 
     printf("%d", line_i + line * bit_width);
-    line_i++;
 
     //はじめに出力した数字の桁数による補正用
     int correction = get_numstr_length(line_i + line * bit_width) - 1;
-    line_i += correction;
+    line_i += correction + 1;
 
     int cell_i;
-    for (cell_i = 1; cell_i < (*seek)->size - correction && line_i < bit_width;
+    for (cell_i = 1;
+         cell_i < (*seek)->unprinted_size - correction && line_i < bit_width;
          cell_i++) {
       printf(" ");
       line_i++;
@@ -179,7 +194,7 @@ void print_line(struct cell **seek, char partition, char padding,
                 enum line_type line_type, int bit_width) {
   int line_i = 0;
   while (line_i < bit_width) {
-    if ((*seek)->size <= 0) {
+    if ((*seek)->unprinted_size <= 0) {
       (*seek) = (*seek)->next;
       break;
     }
@@ -187,8 +202,10 @@ void print_line(struct cell **seek, char partition, char padding,
     line_i++;
 
     int cell_i;
-    for (cell_i = 1; cell_i < (*seek)->size && line_i < bit_width; cell_i++) {
-      if ((cell_i - 1 < (*seek)->name_len) && line_type == Mid) {
+    for (cell_i = 1; cell_i < (*seek)->unprinted_size && line_i < bit_width;
+         cell_i++) {
+      if ((cell_i - 1 < (*seek)->name_len) && line_type == Mid &&
+          (*seek)->is_exceed == false) {
         printf("%c", (*seek)->name[cell_i - 1]);
       } else {
         printf("%c", padding);
@@ -198,7 +215,7 @@ void print_line(struct cell **seek, char partition, char padding,
 
     if (line_i == bit_width) {
       if (line_type == Bottom) {
-        (*seek)->size -= cell_i;
+        (*seek)->unprinted_size -= cell_i;
       }
       printf("%c\n", partition);
       break;
@@ -221,4 +238,30 @@ int get_numstr_length(int num) {
     digit++;
   }
   return digit;
+}
+
+void print_exceed() {
+  printf("---ommitted field---\n");
+  struct cell *tmp = head->next;
+
+  int count = 0;
+
+  while (tmp != NULL) {
+    if (tmp->is_exceed) {
+      if (tmp->width == 1) {
+        int padding = get_numstr_length(count) + 1;
+        for (int i = 0; i < padding; i++) {
+          printf(" ");
+        }
+        printf("%d bit:", count);
+      } else {
+        printf("%d-%d bit:", count, count + tmp->width - 1);
+      }
+      printf("\t%s\n", tmp->name);
+    }
+    count += tmp->width;
+    tmp = tmp->next;
+  }
+
+  printf("--------------------\n");
 }
